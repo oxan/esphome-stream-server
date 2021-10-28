f4108e2f91a901468634cb2b82f1c9ec54c310df
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2021-10-28 20:12:13 +0200

    Drop now-unnecessary import

--- components/stream_server/__init__.py
+++ components/stream_server/__init__.py
@@ -16,7 +16,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
-from esphome.const import CONF_ID, CONF_PORT, __version__
+from esphome.const import CONF_ID, CONF_PORT
 
 # ESPHome doesn't know the Stream abstraction yet, so hardcode to use a UART for now.
 
