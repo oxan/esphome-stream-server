d2bd5687581c25836ba7a328233e6c859413eb60
tube0013 <wpoland@gmail.com>
2026-03-19 09:27:55 -0400

    Update __init__.py
    fix added by

--- components/stream_server/__init__.py
+++ components/stream_server/__init__.py
@@ -1,4 +1,4 @@
-by import esphome.codegen as cg
+import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_PORT, CONF_BUFFER_SIZE
