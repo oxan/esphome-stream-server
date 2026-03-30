9e05b0a3fea78a0ab7c11338327e5492f0c95b43
dv49273 <fjorde-gaze.8y@icloud.com>
2026-03-30 12:39:16 -0400

    Fix no connection under ESPHome 2026.3

--- components/stream_server/stream_server.h
+++ components/stream_server/stream_server.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "esphome/core/version.h"
 #include "esphome/core/component.h"
 #include "esphome/components/socket/socket.h"
 #include "esphome/components/uart/uart.h"
@@ -75,6 +76,11 @@ protected:
     size_t buf_head_{0};
     size_t buf_tail_{0};
 
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 3, 0)
+    esphome::socket::ListenSocket *socket_{nullptr};
+#else
     std::unique_ptr<esphome::socket::Socket> socket_{};
+#endif 
+
     std::vector<Client> clients_{};
 };
